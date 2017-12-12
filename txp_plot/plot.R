47e7316dda225f1234f91eb60c1d4627bcd82c60
aksinghal5590 <aksinghal@cs.stonybrook.edu>
2017-12-12 16:03:29 -0500

    Updated plot

--- txp_plot/plot.R
+++ txp_plot/plot.R
@@ -23,17 +23,17 @@ if(length(args) < 2) {
 
 png(file = outputFile, width=1000, height=600, unit="px")
 
-plot(i_vec, type = "l", col = "blue", axes=FALSE, ann=FALSE)
+plot(i_vec, type = "l", col = "blue", lty = 1, axes=FALSE, ann=FALSE)
 
 xLabCount = as.integer(length(i_vec)/20)
 yLabCount = as.integer(max(i_vec)/8)
 axis(1, at = xLabCount * 0:length(i_vec))
 axis(2, las = 1, at = yLabCount * 0:max(i_vec))
-box()
 title(main="Transcript Read Coverage", font.main=4)
-title(xlab="Locus", col.lab = rgb(0,0.5,0))
-title(ylab="Coverage", col.lab = rgb(0,0.5,0))
-legend("topright", c(c_vec[1]), cex=1.0, bty="n")
+title(xlab="Locus")
+title(ylab="Coverage")
+legend("topright", c(c_vec[1]), cex = 1.0, col = c("blue"), lty = 1)
+box(bty="L")
 
 dev.off()
 #df <- data.frame(i_vec)
